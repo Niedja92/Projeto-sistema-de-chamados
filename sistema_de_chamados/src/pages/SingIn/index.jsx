3bab4ba9555266688fd233753c2bd212d4d8eed4
Niedja92 <niedleticia@hotmail.com>
2023-12-10 17:10:00 -0300

    login-page

---
+++ sistema_de_chamados/src/pages/SingIn/index.jsx
@@ -0,0 +1,55 @@
+import { useState, useContext } from "react";
+import "./singIn.css";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/auth";
+
+import logo from "../../assets/logo.png";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useContext(AuthContext);
+
+  function handleSignIn(e) {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      signIn(email, password);
+    }
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+
+        <form onSubmit={handleSignIn}>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
+}
+
+// 1º passo para o início deste projeto: criar páginas de login e cadastro
+// o Link do react-router-dom fará com que, ao clicar, o usuário seja direcionado ao caminho desejado
