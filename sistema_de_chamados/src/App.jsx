5286aa0c42386cf743eb9e757e3324237eb0dd6a
Niedja92 <niedleticia@hotmail.com>
2023-12-10 17:05:35 -0300

    first-commit-and-import-of-routes

--- sistema_de_chamados/src/App.jsx
+++ sistema_de_chamados/src/App.jsx
@@ -1,8 +1,19 @@
+import "./index.css";
+
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
+import AuthProvider from "./contexts/auth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
-    <>
-      <h1>Testando...</h1>
-    </>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastContainer autoClose={3000} />
+        <RoutesApp />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
