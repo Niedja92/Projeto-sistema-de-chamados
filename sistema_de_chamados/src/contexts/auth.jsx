3bd0e4e29bdb3a8bacefaa4092f49e598ace08c5
Niedja92 <niedleticia@hotmail.com>
2023-12-11 17:26:42 -0300

    finalizing-authentication-settings

--- sistema_de_chamados/src/contexts/auth.jsx
+++ sistema_de_chamados/src/contexts/auth.jsx
@@ -1,6 +1,10 @@
 import { auth, db } from "../services/firebaseConection";
-import { useState, createContext } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState, createContext, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -10,13 +14,52 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
 
-  function signIn(email, password) {
-    console.log(email);
-    console.log(password);
-    alert("LOGADO COM SUCESSO");
+  useEffect(() => {
+    async function loadUser() {
+      const storageUser = localStorage.getItem("@ticketsPRO");
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+
+      setLoading(false);
+    }
+    loadUser();
+  }, []);
+
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        const docRef = doc(db, "users", uid);
+        const docSnap = await getDoc(docRef);
+
+        let data = {
+          uid: uid,
+          nome: docSnap.data().nome,
+          email: value.user.email,
+          avatarUrl: docSnap.data().avatarUrl,
+        };
+
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+        toast.success("Bem-vindo(a) de volta!");
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoadingAuth(false);
+        toast.error("Ops! Algo deu errado.");
+      });
   }
 
   // Cadastrar novo usuário
@@ -42,7 +85,7 @@ function AuthProvider({ children }) {
             setUser(data);
             storageUser(data);
             setLoadingAuth(false);
-            toast.success("Seja bem-vindo ao sistema!");
+            toast.success("Seja bem-vindo(a) ao sistema!");
             navigate("/dashboard");
           })
           .catch((error) => {
@@ -57,6 +100,12 @@ function AuthProvider({ children }) {
     localStorage.setItem("@ticketsPRO", JSON.stringify(data));
   }
 
+  async function logout() {
+    await signOut(auth);
+    localStorage.removeItem("@ticketsPRO");
+    setUser(null);
+  }
+
   return (
     <AuthContext.Provider
       value={{
@@ -64,7 +113,9 @@ function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
+        logout,
         loadingAuth,
+        loading,
       }}
     >
       {children}
