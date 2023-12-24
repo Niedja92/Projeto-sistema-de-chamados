fccf53fb80830ab6c902a96afa833373e9beedad
Niedja92 <niedleticia@hotmail.com>
2023-12-23 23:12:02 -0300

    updating the new and dashboard components

--- sistema_de_chamados/src/pages/Dashboard/index.jsx
+++ sistema_de_chamados/src/pages/Dashboard/index.jsx
@@ -1,17 +1,65 @@
 import "./dashboard.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  limit,
+  startAfter,
+  query,
+} from "firebase/firestore";
+import { db } from "../../services/firebaseConection";
+
+const listRef = collection(db, "chamados");
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
 
-  async function handleLogout() {
-    await logout();
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    async function loadChamados() {
+      const q = query(listRef, orderBy("created", "desc"), limit(5));
+      const querySnapshot = await getDocs(q);
+      await updateState(querySnapshot);
+      setLoading(false);
+    }
+
+    loadChamados();
+
+    return () => {};
+  }, []);
+
+  async function updateState(querySnapshot) {
+    const isCollectionEmpty = querySnapshot.size === 0;
+
+    if (!isCollectionEmpty) {
+      let lista = [];
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          status: doc.data().status,
+          complemento: doc.data().complemento,
+        });
+      });
+
+      setChamados((chamados) => [...chamados, ...lista]);
+    } else {
+      setIsEmpty(true);
+    }
   }
+
   return (
     <div>
       <Header />
@@ -21,48 +69,60 @@ export default function Dashboard() {
         </Title>
 
         <>
-          <Link to="/new" className="new">
-            <FiPlus color="#fff" size={25} />
-            Novo chamado
-          </Link>
+          {chamados.length === 0 ? (
+            <div>
+              <span>Nenhum chamado encontrado...</span>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Assunto</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Cadastrado em</th>
+                    <th scope="col">#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td data-label="Cliente">Mercado</td>
+                    <td data-label="Assunto">Suporte</td>
+                    <td data-label="Status">
+                      <span
+                        className="badge"
+                        style={{ backgroundColor: "#999" }}
+                      >
+                        Em aberto
+                      </span>
+                    </td>
+                    <td data-label="Cadastrado">13/06/2022</td>
+                    <td data-label="#">
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#3583f6" }}
+                      >
+                        <FiSearch color="#fff" size={25} />
+                      </button>
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#f6a935" }}
+                      >
+                        <FiEdit2 color="#fff" size={25} />
+                      </button>
+                    </td>
+                  </tr>
 
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Cliente</th>
-                <th scope="col">Assunto</th>
-                <th scope="col">Status</th>
-                <th scope="col">Cadastrado em</th>
-                <th scope="col">#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td data-label="Cliente">Mercado</td>
-                <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">
-                  <span className="badge" style={{ backgroundColor: "#999" }}>
-                    Em aberto
-                  </span>
-                </td>
-                <td data-label="Cadastrado">13/06/2022</td>
-                <td data-label="#">
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#3583f6" }}
-                  >
-                    <FiSearch color="#fff" size={25} />
-                  </button>
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#f6a935" }}
-                  >
-                    <FiEdit2 color="#fff" size={25} />
-                  </button>
-                </td>
-              </tr>
-
-              <tr>
+                  {/* <tr>
                 <td data-label="Cliente">Info TECH</td>
                 <td data-label="Assunto">Suporte</td>
                 <td data-label="Status">
@@ -85,9 +145,11 @@ export default function Dashboard() {
                     <FiEdit2 color="#fff" size={25} />
                   </button>
                 </td>
-              </tr>
-            </tbody>
-          </table>
+              </tr> */}
+                </tbody>
+              </table>
+            </>
+          )}
         </>
       </div>
     </div>
--- sistema_de_chamados/src/pages/New/index.jsx
+++ sistema_de_chamados/src/pages/New/index.jsx
@@ -2,18 +2,89 @@ import { FiPlusCircle } from "react-icons/fi";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import "./new.css";
-import { useState } from "react";
-import { loadBundle } from "firebase/firestore";
+import { useState, useEffect, useContext } from "react";
+import { db } from "../../services/firebaseConection";
+import { collection, getDoc, getDocs, doc, addDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+import { AuthContext } from "../../contexts/auth";
+
+const listRef = collection(db, "customers");
 
 export default function New() {
+  const { user } = useContext(AuthContext);
+
   const [customers, setCustomers] = useState([]);
+  const [loadCustomer, setLoadCustomer] = useState(true);
+  const [customerSelected, setCustomerSelected] = useState(0);
 
   const [complemento, setComplemento] = useState("");
   const [assunto, setAssunto] = useState("Suporte");
   const [status, setStatus] = useState("Aberto");
 
+  useEffect(() => {
+    async function loadCustomers() {
+      const querySnapshot = await getDocs(listRef)
+        .then((snapshot) => {
+          let lista = [];
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              nomeFantasia: doc.data().nomeFantasia,
+            });
+          });
+          if (snapshot.docs.size === 0) {
+            console.log("NENHUMA EMPRESA ENCONTRADA");
+            setCustomers([{ id: 1, nomeFantasia: "FREELA" }]);
+            setLoadCustomer(false);
+            return;
+          }
+
+          setCustomers(lista);
+          setLoadCustomer(false); // para a execução do LoadCustomer
+        })
+        .catch((error) => {
+          setLoadCustomer(false);
+          setCustomers([{ id: 1, nomeFantasia: "FREELA" }]);
+        });
+    }
+  }, []);
+
   function handleOptionChange(e) {
     setStatus(e.target.value);
+  }
+
+  function handleChangeSelect(e) {
+    setStatus(e.target.value);
+  }
+
+  function handleChangeCustomer(e) {
+    setCustomerSelected(e.target.value);
+    console.log(customers[e.target.value].nomeFantasia);
+  }
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    // Registrar um chamado
+    await addDoc(collection(db, "chamados"), {
+      created: new Date(),
+      cliente: customers[customerSelected].nomeFantasia,
+      clienteId: customers[customerSelected].id,
+      assunto: assunto,
+      complemento: complemento,
+      status: status,
+      userId: user.uid,
+    })
+      .then(() => {
+        toast.success("Chamado registrado!");
+        setComplemento("");
+        setCustomerSelected(0);
+      })
+      .catch((error) => {
+        toast.error("Ops! Erro ao registrar, tente mais tarde!");
+        console.log(error);
+      });
   }
 
   return (
@@ -26,19 +97,24 @@ export default function New() {
         </Title>
 
         <div className="container">
-          <form className="form-profile">
+          <form className="form-profile" onSubmit={handleRegister}>
             <label>Clientes</label>
-            <select>
-              <option key={1} value={1}>
-                Mercado
-              </option>
-              <option key={2} value={2}>
-                Loja informática
-              </option>
-            </select>
+            {loadCustomer ? (
+              <input type="text" disabled={true} value="Carregando..." />
+            ) : (
+              <select value={customerSelected} onChange={handleChangeCustomer}>
+                {customers.map((item, index) => {
+                  return (
+                    <option key={index} value={index}>
+                      {item.nomeFantasia}
+                    </option>
+                  );
+                })}
+              </select>
+            )}
 
             <label>Assunto</label>
-            <select>
+            <select value={assunto} onChange={handleChangeSelect}>
               <option value="Suporte">Suporte</option>
               <option value="Visita Técnica">Visita Técnica</option>
               <option value="Financeira">Financeira</option>
